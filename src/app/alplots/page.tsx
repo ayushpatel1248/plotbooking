b65dc7b6fcafbacbdf5dcfaf0360a9936db99102
ayush <ayushpatel1248@gmail.com>
2024-11-15 00:07:03 +0530

    s

--- src/app/alplots/page.tsx
+++ src/app/alplots/page.tsx
@@ -300,7 +300,7 @@ const Example = () => {
       <header className="mb-20">
         <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-            <a href="https://flowbite.com" className="flex items-center">
+            <a href="" className="flex items-center">
 
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
                 Plotify
@@ -354,7 +354,7 @@ const Example = () => {
               <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                   <a
-                    href="#"
+                    href="/home"
                     className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                     aria-current="page"
                   >
@@ -363,7 +363,7 @@ const Example = () => {
                 </li>
                 <li>
                   <a
-                    href="#"
+                    href="allplot"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     All Plots
